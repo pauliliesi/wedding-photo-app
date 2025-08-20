8e37863ee3a44ddbc6ced502f55dd9e273fe410a
Iliesi Paul <i.paulnick@yahoo.com>
2025-08-20 21:32:14 +0300

    c

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    files: ["/*.ts", "/*.tsx"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      // alternativ: doar avertisment și ignori argumentele/variabilele prefixate cu _
-      // "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^", varsIgnorePattern: "^", ignoreRestSiblings: true }],
-    },
-  },
-];
-
-export default eslintConfig;
+export default defineConfig([
+	{
+		rules: {
+			"no-unused-vars": "off",
+			"no-explicit-any": "off",
+      "allowObjectTypes": true,
+      "no-empty-object-type": "off"
+		},
+	},
+]);
